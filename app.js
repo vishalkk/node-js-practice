6500fdab776a0540a45017cade24575f0b2094d1
vishal <vishalkajales@gmail.com>
2023-05-23 07:52:39 +0530

    # learn path module

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const log = require('./logger');
-log("Good morning vishal )(:(");
+// log("Good morning vishal )(:(");
+
+//path module provided by node
+const path = require('path');
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
